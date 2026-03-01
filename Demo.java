956142b96ef2b6e42049ab100205f682293f09c0
Hanush <penchalahanushboddu@gmail.com>
2026-03-01 20:04:31 +0530

    Hanush

--- Demo.java
+++ Demo.java
@@ -1,15 +1,5 @@
 class Demo{
 	public static void main(String args[]){
 	System.out.println("OOPS");
-	 // Hint: Use the Example below as a guide to construct each line of the banner
-     // Example for OS which stands for Operating System:
-
-    System.out.println("   ***   " + " " + "  ***** ");
-    System.out.println("  **  ** " + " " + " **     ");
-    System.out.println(" **    **" + " " + "**      ");
-    System.out.println(" **    **" + " " + " *****  ");
-    System.out.println(" **    **" + " " + "    **  ");
-    System.out.println("  **  ** " + " " + " **  ** ");
-    System.out.println("   ***   " + " " + "  ***** ");
     }
 }
